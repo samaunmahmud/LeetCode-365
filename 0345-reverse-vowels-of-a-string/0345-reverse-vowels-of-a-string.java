7bffe71c8665d89db02051f2734234e3475f4b12
Samaun Mahmud <samaunmahmud9@gmail.com>
2026-08-19 19:38:41 +0100

    Time: 2 ms (99.35%), Space: 46.8 MB (21.69%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,30 +1,66 @@
-import java.util.Stack;
-
 class Solution {
     public String reverseVowels(String s) {
-        Stack<Character> rev = new Stack<>();
         char[] ch = s.toCharArray();
+        int left = 0;
+        int right = ch.length - 1;
 
-    
-        for (char c : ch) {
-            if (isVowel(c)) {
-                rev.push(c);
+        while (left < right) {
+            while (left < right && !isVowel(ch[left])) {
+                left++;
             }
-        }
+            while (left < right && !isVowel(ch[right])) {
+                right--;
+            }
 
-       
-        for (int i = 0; i < ch.length; i++) {
-            if (isVowel(ch[i])) {
-                ch[i] = rev.pop();
-            }
+            // Swap vowels
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+
+            left++;
+            right--;
         }
 
         return new String(ch);
     }
 
- 
     private boolean isVowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
 }
+
+
+
+
+
+
+
+
+
+// public String reverseVowels(String s) {
+//         Stack<Character> rev = new Stack<>();
+//         char[] ch = s.toCharArray();
+
+    
+//         for (char c : ch) {
+//             if (isVowel(c)) {
+//                 rev.push(c);
+//             }
+//         }
+
+       
+//         for (int i = 0; i < ch.length; i++) {
+//             if (isVowel(ch[i])) {
+//                 ch[i] = rev.pop();
+//             }
+//         }
+
+//         return new String(ch);
+//     }
+
+ 
+//     private boolean isVowel(char c) {
+//         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+//                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+//     }

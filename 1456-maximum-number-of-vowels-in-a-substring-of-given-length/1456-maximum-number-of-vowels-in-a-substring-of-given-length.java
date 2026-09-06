e4fbbd8097431754bc04af71b5159e2a9eef586b
Samaun Mahmud <samaunmahmud9@gmail.com>
2026-09-06 22:00:36 +0100

    Time: 11 ms (91.6%), Space: 46.7 MB (17.64%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -44,3 +44,23 @@ class Solution {
         return false;
     }
 }
+
+
+/**
+ * How the Sliding Window Algorithm Works for maxVowels:
+ * 
+ * 1. Preprocessing:
+ *    - We convert the string to a lowercase character array ('holder') 
+ *      to avoid case-sensitivity issues and extra overhead during loops.
+ * 
+ * 2. First Window (Initialization):
+ *    - We loop from index 0 to k - 1 to count the vowels in the 
+ *      initial window of size 'k', storing it in 'currentVowels' 
+ *      and setting our initial 'max'.
+ * 
+ * 3. Sliding Phase:
+ *    - As the loop moves 'i' from 'k' to the end of the array, the window shifts right by 1 index.
+ *    - We remove the old character: If the character leaving on the left (index i - k) was a vowel, we decrement 'currentVowels'.
+ *    - We add the new character: If the new character entering on the right (index i) is a vowel, we increment 'currentVowels'.
+ *    - We update 'max': We compare the updated 'currentVowels' with 'max' using Math.max() to keep the highest count.
+ */
